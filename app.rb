15f24652e323795e0d689c41483699c436b9ddd1
Christian Fredericksen <christian.fredericksen@yahoo.com>
2020-03-18 19:26:13 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -17,7 +17,7 @@ class App < Sinatra::Base
     @number = params[:number].to_i
     @phrase = params[:phrase]
     @number.times do
-      @product += "@phrase\n"
+      @product += "#{@phrase}\n"
       #@product += "\n"
     end
     "#{@product}"
