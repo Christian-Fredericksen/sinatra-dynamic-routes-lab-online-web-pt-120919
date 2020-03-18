0093acf797ac817c3cf68d16fa2d83729f14e737
Christian Fredericksen <christian.fredericksen@yahoo.com>
2020-03-18 19:27:24 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,27 +12,27 @@ class App < Sinatra::Base
     "#{@result}"
   end
   
-  get '/say/:number/:phrase' do
-    @product = ""
-    @number = params[:number].to_i
-    @phrase = params[:phrase]
-    @number.times do
-      @product += "#{@phrase}\n"
-      #@product += "\n"
-    end
-    @product
-  end
-  
-  
-  # get "/say/:number/:phrase" do
-  #   @the_product = ""
-  #   @the_number = params[:number].to_i
-  #   @the_phrase = params[:phrase]
-  #   @the_number.times do
-  #     @the_product += @the_phrase
-  #     @the_product += "\n"
+  # get '/say/:number/:phrase' do
+  #   @product = ""
+  #   @number = params[:number].to_i
+  #   @phrase = params[:phrase]
+  #   @number.times do
+  #     @product += "#{@phrase}\n"
+  #     #@product += "\n"
   #   end
-  #   "#{@the_product}"
+  #   @product
   # end
+  
+  
+  get "/say/:number/:phrase" do
+    @the_product = ""
+    @the_number = params[:number].to_i
+    @the_phrase = params[:phrase]
+    @the_number.times do
+      @the_product += @the_phrase
+      @the_product += "\n"
+    end
+    "#{@the_product}"
+  end
   
 end
