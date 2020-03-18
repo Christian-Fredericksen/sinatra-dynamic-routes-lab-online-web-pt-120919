8a220970d62ad528e2e21f164ee92dc031c388d0
Christian Fredericksen <christian.fredericksen@yahoo.com>
2020-03-18 20:23:25 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -28,6 +28,7 @@ class App < Sinatra::Base
   end
   
   get '/:operation/:number1/:number2' do 
-    "#{params[:operation]} #{params[:number1]} #{params[:number2]}"
+    @result = params[:number1].to_i params[:operation] params[:number].to_i
+    "#{@result}"
   end 
 end
