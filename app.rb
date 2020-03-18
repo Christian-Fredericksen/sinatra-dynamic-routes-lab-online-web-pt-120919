d3e8ae918297f3b123bbc90183d038478495b1b3
Christian Fredericksen <christian.fredericksen@yahoo.com>
2020-03-18 20:43:47 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -33,6 +33,7 @@ class App < Sinatra::Base
     @number_2 = params[:number2].to_i
     if params[:operation] == "add"
       @answer += "#{@number_1+@number_2}"
+      
     elsif params[:operation] == "subtract"
       @answer += "#{@number_1-@number_2}"
     elsif params[:operation] == "multiply"
